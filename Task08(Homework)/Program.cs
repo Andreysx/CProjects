192cd39d85e680a9ba791d59f981f5a744f21f34
Andreye <drontop1999@mail>
2023-01-17 19:54:35 +0300

    add Task08(Homework)

---
+++ Task08(Homework)/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 8:Напишите программу 
+//которая на вход принимает число(N) 
+//а на выходе показывает все четные числа от 1 до (N)
+ 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= number)
+{
+if (count % 2 == 0)
+Console.Write(count+ " ");
+count++;
+}
