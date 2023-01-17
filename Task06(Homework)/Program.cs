c05f7761e46b2419a999d97d09b5b4c26d43fa76
Andreye <drontop1999@mail>
2023-01-17 19:51:09 +0300

    add Task06(Homework)

---
+++ Task06(Homework)/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4:Напишите программу 
+//которая на вход принимает три числа 
+//и выдает максимальное из этих чисел
+ 
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
