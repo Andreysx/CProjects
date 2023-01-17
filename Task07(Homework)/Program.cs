eaff8979efb0790157666d1b34e44e952aa7ba69
Andreye <drontop1999@mail>
2023-01-17 19:52:46 +0300

    add Task07(Homework)

---
+++ Task07(Homework)/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 6:Напишите программу которая на вход принимает число и выдает
+//является ли число четным(делится на 2 без остатка)
+ 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
