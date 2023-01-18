d81ad335b4e1ff1d2b9de91409fe1115f0dc431e
Andreye <drontop1999@mail>
2023-01-18 23:42:52 +0300

    Added  task13

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+ 
+bool Multiplicity(int number1, int number2)
+{
+return number1*number1 == number2 || number2*number2 == number1;
+}
+ 
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+bool res = Multiplicity(num1, num2);
+Console.WriteLine (res ? $"{num1},{num2} -> Да": $"{num1},{num2} -> Нет");
