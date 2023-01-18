114375520640b8bbd75884a8cb121124bb8a18d2
Andreye <drontop1999@mail>
2023-01-18 23:20:00 +0300

    Внесли изменения в task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -5,10 +5,10 @@
 //34, 5 -> не кратно, остаток 4
 //16, 4 -> кратно
 
-Console.Write("Введите первое целое число A: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе целое число B: ");
-int b = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите первое целое число A: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите второе целое число B: ");
+//int b = Convert.ToInt32(Console.ReadLine());
 
 //if (a % b == 0)
 //{
@@ -26,3 +26,26 @@ int b = Convert.ToInt32(Console.ReadLine());
 //{
  //   return a % b;
 //}
+
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 % num2 == 0)
+// {
+// Console.WriteLine($"{num1} кратно {num2}");
+// }
+// else
+// {
+// Console.WriteLine($"{num1} не кратно {num2} , остаток {num1 % num2}");
+// }
+
+int remains = Remains( num1,num2);
+Console.WriteLine($"кратно ");
+
+int Remains(int number1,int number2)
+{
+return number1 % number2;//ПРОГРАММА НЕ ДОКОНЦА НАПИСАНА!!!
+}
