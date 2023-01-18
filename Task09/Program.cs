21441db73822b74f3ccc7ac0e4b058a01685234f
Andreye <drontop1999@mail>
2023-01-18 22:26:58 +0300

    Внесли изменения в task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,30 +1,35 @@
-﻿// Задача 9: Напишите программу, которая выводит
-//случайное число из отрезка [10, 99] и показывает
-//наибольшую цифру числа.
+﻿// Задача 9: Напишите программу, 
+//которая выводит случайное число
+//из отрезка [10, 99] и 
+//показывает наибольшую цифру числа.
 //78 -> 8
 //12-> 2
 
-int number = new Random().Next(10,100);
-Console.WriteLine($"Случайное число из отрезка 10 - 99-> {number}");
+int number = new Random().Next(10, 100);// Random- выделение памяти, Next метод (параметры диапазона)
+Console.WriteLine($"Случайное число из отрезка 10 - 99-> {number}");// Выводит случ число из данного диапазона
 
-//int firstDigit = number / 10; //7
-//int secondDigit = number % 10;//8
+//int firstDigit = number / 10; //7 получаем переменную(делим без остатка)(целочисленное деление)
+//int secondDigit = number % 10;//8 остаток от деления
 
-// int result  = firstDigit > secondDigir ? firstDigit: secondDigit; тернарный оператор тоже решение задачи
+//if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}"); Простое решение задачи 9 ветвлением(условный оператор)
+//else Console.WriteLine($"Наибольшая  цифра числа -> {secondDigit}"); Простое решение задачи 9  ветвлением(условный оператор)
+
+// int result  = firstDigit > secondDigit ? firstDigit: secondDigit; тернарный оператор
+//( (?)если выполняется условие first возвращаем иначе(:) second ) тоже решение задачи 9
 //Console.WriteLine($"Наибольшая цифра числа -> {result}");
 
-//int result = firstDigit > secondDigir ? firstDigit : secondDigit; //тернарный оператор тоже решение задачи
+//int result = firstDigit > secondDigit ? firstDigit : secondDigit; //тернарный оператор тоже решение задачи 9
 //Console.WriteLine($"Наибольшая цифра числа -> {result}");
 
-int maxDigit = MaxDigit(number);
+int maxDigit = MaxDigit(number);// метож MaxDigit возвращает значение в переменную maxDigit (исходные данные=> numder)
 Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
 
 int MaxDigit(int number)
 {
-    
+
     int firstDigit = number / 10; //7
     int secondDigit = number % 10;//8
     int result = firstDigit > secondDigit ? firstDigit : secondDigit;
-    return 0;
+    return result;
 
 }
