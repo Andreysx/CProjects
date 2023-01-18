fb67f3e4eeab0058afa0d32fb145a3690ecc3bc3
Andreye <drontop1999@mail>
2023-01-18 14:48:54 +0300

    Внесли изменения в task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,18 +4,18 @@
 //456 -> 46
 //782 -> 72
 
-int number = new Random().Next(100,1000);
-Console.WriteLine($"Случайное число из отрезка 100 - 1000-> {number}");
+//int number = new Random().Next(100,1000);
+//Console.WriteLine($"Случайное число из отрезка 100 - 1000-> {number}");
 
-int res = RemoveSecondDigit(number);
-Console.WriteLine($"рузультат -> {res}");
-int First(int num)
-{
-    int num1 = number / 100;
-    int num3 = number % 10;
-    int result = num1 * 10 + num3;
-    return result;
-}
+//int res = RemoveSecondDigit(number);
+//Console.WriteLine($"рузультат -> {res}");
+//int First(int num)
+//{
+//    int num1 = number / 100;
+ //   int num3 = number % 10;
+ //   int result = num1 * 10 + num3;
+ //   return result;
+//}
 
 
 
