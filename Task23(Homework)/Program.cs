b962821889c86e54ddacf70d56ee1a7357c6c0f9
Andreye <drontop1999@mail>
2023-01-25 19:32:05 +0300

    Added Task23(homework)

---
+++ Task23(Homework)/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Задача 23
+//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void SquareTable(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count,5} -> {count * count * count}");// 5- форматирование строки
+        count++;
+    }
+
+}
+SquareTable(number);
