24e1b60dc4ef866b5d36fbe46ccecf421de9faf4
Andreye <drontop1999@mail>
2023-01-26 19:37:46 +0300

    Draft

---
+++ Draft/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a >= 1 && a <= 7)
+{
+    Console.Write($"{WeekDay(a)}");
+}
+else
+{
+    Console.Write("Это число не совпадает c днем недели");
+}
+bool WeekDay(int num)
+{
+    if (num == 7 || num == 6)
+    {
+        Console.Write($"{num} -> Да");
+    }
+    else
+    {
+        Console.Write($"{num} -> Нет");
+    }
+}
+
+
+
+
+
