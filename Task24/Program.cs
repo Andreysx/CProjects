0ccf7a2bba9d22811656222166e71d2383726825
Andreye <drontop1999@mail>
2023-01-26 19:36:13 +0300

    изменения в Task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -5,7 +5,7 @@
 
 
 int[] array = new int[8];//0,1,2,3,4,5,6,7
-
+//array[0] = 123; пример обращения к массиву и положить в него значение
 
 void PrintArray(int[] arr)
 {
@@ -24,7 +24,7 @@ void FillArray(int[] arr)
 
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(0, 2);
+        arr[i] = new Random().Next(0, 2);// у нас диапазон от 0 до 1(min,max), но в диапазоне последнее число не включаетмя в диапазон
     }
 }
 
@@ -39,13 +39,14 @@ PrintArray(array);
 
 // int[] CreateArray(int size)
 // {
-//     int[] array = new int[size];
-//     Random rnd = new Random();
+//     int[] array = new int[size];ТУТ МЫ СОЗДАЛИ МАССИВ ВНУТРИ МЕТОДА
+
+//     Random rnd = new Random(); - ТАК ПРАВИЛЬНЕЕ СВЯЗАНО СО СКОРОСТЬЮ, ТАКТЫ ВРЕМЕНИ = 15МС И ЦИКЛ ВЫПОЛНЯЕТСЯ БЫСТРЕЕ 15МС
 //     for (int i = 0; i < size; i++)
 //     {
 //         array[i] = rnd.Next(0, 2);
 //     }
-//     return array;
+//     return array;И ВЕРНУЛИ ЕГО
 // }
 
 
