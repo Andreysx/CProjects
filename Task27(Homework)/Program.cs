535573e981b295728bf93de36a5d8f3e19febbb5
Andreye <drontop1999@mail>
2023-01-27 22:52:30 +0300

    Task27(Homework)

---
+++ Task27(Homework)/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+// }
+int[] array = FillArray(8);
+
+PrintArray(array);
+Console.Write(" -> ");
+PrintArray2(array);
+
+
+
+int[] FillArray(int arr)
+{
+    int[] arrayy = new int[arr];
+
+    Random rnd = new Random();
+    for (int i = 0; i < arr; i++)
+    {
+        arrayy[i] = rnd.Next(10, 100);
+    }
+    return arrayy;
+}
+
+
+void PrintArray(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + ",");
+        // Console.Write(string.Join(arr[i] + ","));
+    }
+
+}
+void PrintArray2(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + ",");
+        // Console.Write(string.Join(arr[i] + ","));
+    }
+    Console.Write("]");
+}
