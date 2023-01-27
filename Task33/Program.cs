158561a9d66c6ebecf2f76f2ace8f72bdc52a43a
Andreye <drontop1999@mail>
2023-01-27 22:53:59 +0300

    Task33

---
+++ Task33/Program.cs
@@ -0,0 +1,22 @@
+﻿double[] CreateArrayRndInt(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max-min) + min;//округление
+        arr[i] = Math.Round(num, 1);//округление
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
