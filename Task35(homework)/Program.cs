d7788b0702faaa00920c74a57af3c09ce34bec78
Andreye <drontop1999@mail>
2023-01-30 14:13:31 +0300

    Task35(homework)

---
+++ Task35(homework)/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+int[] array = CreateArrayRndInt(6, -10, 50);
+PrintArray(array);
+Console.Write(" -> ");
+Console.WriteLine($"{SummOddPosition(array)}");
+
+
+
+int SummOddPosition(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum = arr[i] + sum;
+            i++;
+        }
+    }
+    return sum;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
