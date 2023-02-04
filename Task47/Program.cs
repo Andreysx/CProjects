6c6bfa30942c027b9870a76ae3888328cf13a862
Andreye <drontop1999@mail>
2023-02-04 14:18:34 +0300

    Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,88 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+
+
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");// 4--- это форматирование для вывода массива
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+
+
+// int SumMatrixElement(int[,] matrix)//вариант без оптимизации решения
+// {
+//     int sum = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i == j)
+//             {
+//                 sum += matrix[i, j];
+
+//             }
+//         }
+
+//     }
+//     return sum;
+// }
+
+
+
+int SumMatrixElement(int[,] matrix)//вариант с оптимизацией решения
+{
+    int sum = 0;
+    int size = matrix.GetLength(0);
+    if (matrix.GetLength(0) > matrix.GetLength(1))
+        size = matrix.GetLength(1);
+    for (int i = 0; i < size; i++)
+    {
+
+        sum += matrix[i, i];
+    }
+    return sum;
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+Console.WriteLine("   ");
+int result = SumMatrixElement(array2D);
+Console.WriteLine(result);
