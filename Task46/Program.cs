e5c514b81684de683fbb40e17707deaf32d6cea8
Andreye <drontop1999@mail>
2023-02-04 14:18:27 +0300

    Task46

---
+++ Task46/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");// 4--- это форматирование для вывода массива
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+// void SquareAddNumbers(int[,] matrix)
+// {
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i % 2 == 0 && j % 2 == 0)
+//             {
+//                 matrix[i, j] = matrix[i, j] * matrix[i, j];
+
+//             }
+//         }
+
+//     }
+// }
+
+
+
+void SquareAddNumbers(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)// i += 2 -- проходим цикл по четным элементам
+        {
+
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+
+    }
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+Console.WriteLine("   ");
+SquareAddNumbers(array2D);
+PrintMatrix(array2D);
+
