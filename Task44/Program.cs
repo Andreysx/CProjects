c494dfb7148b81d4822921ca68c26ba256755669
Andreye <drontop1999@mail>
2023-02-04 14:18:11 +0300

    Task44

---
+++ Task44/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+//        0   1   2   3 ИНДЕКСЫ
+
+// 0      1   4   8   19
+// 1      5   -2   33  -2
+// 2      77   3   8   1
+
+
+
+
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];///  0,1,
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)//цикл проходит по строкам// чтобы пройтись по столбцам нужно поменять цикли и i и j
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)/// циул проходит по столбцам
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} ");// 4--- это форматирование для вывода массива
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+int[,] array2D = CreateMatrixRndInt(3,4,-100,100);
+PrintMatrix(array2D);
+
+
+
+// int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+// {
+//     int[,] matrix = new int[rows, columns]; // 0, 1
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         Console.Write("|");
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} | ");
+//             else Console.Write($"{matrix[i, j], 4} ");
+//         }
+//         Console.WriteLine("|");
+//     }
+// }
+
+// int[,] array2D = CreateMatrixRndInt(3, 4, -100, 100);
+// PrintMatrix(array2D);
