9ed332cba3276423c6156a2d2c2d077ee43262fa
Andreye <drontop1999@mail>
2023-02-05 21:11:56 +0300

    changed  Task49(Homework)

--- Task49(Homework)/Program.cs
+++ Task49(Homework)/Program.cs
@@ -12,35 +12,9 @@
 // 1, 7 -> такого элемента в массиве нет
 
 
-Console.Write("Введите номер строки: ");
-int row = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите номер столбца: ");
-int column = Convert.ToInt32(Console.ReadLine());
-
-int[,] mass = CreateMatrixRndInt(3, 4, -10, 10);
-PrintMatrix(mass);
-Console.WriteLine("");
-ReturnValueElement(mass, row, column);
-
-
-void ReturnValueElement(int[,] inArray, int inRows, int inColums)
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)//метод задает двумерный массив целых чисел (наш шаблонный)
 {
-    if (inRows >= 0 && inColums >= 0)
-    {
-        if (inRows < inArray.GetLength(0) && inColums < inArray.GetLength(1))
-        {
-            Console.Write($"{inRows}, {inColums} -> {inArray[inRows, inColums]}");
-        }
-        else
-            Console.Write($"[{inRows}, {inColums}] -> такого элемента в массиве нет");
-    }
-    else 
-     Console.Write("Введено некорректное значение");
-}
-
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
-{
-    int[,] matrix = new int[rows, columns];///  0,1,
+    int[,] matrix = new int[rows, columns];/// 0,1,
     Random rnd = new Random();
 
     for (int i = 0; i < matrix.GetLength(0); i++)//цикл проходит по строкам// чтобы пройтись по столбцам нужно поменять цикли и i и j
@@ -53,17 +27,40 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
     }
     return matrix;
 }
-
-void PrintMatrix(int[,] matrix)
+void PrintMatrix(int[,] matrix)//метод распечатывает двумерный массив целых чисел(наш шаблонный)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        Console.Write("|");
+        Console.Write("||");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} |");
             else Console.Write($"{matrix[i, j],4} ");
         }
-        Console.WriteLine("|");
+        Console.WriteLine("||");
     }
 }
+void ReturnMeaning(int[,] matrix, int line, int colum)//метод который возвращает значение элемента на указанных позициях или сообщает что такого элемента нет
+{
+    if (line >= 0 && colum >= 0)//проверка на корректность ввода данных(отрицательные значения)
+    {
+        if (line < matrix.GetLength(0) && colum < matrix.GetLength(1))//проверяем не выходят ли введенные данные за пределы массива
+        {
+            Console.Write($"[{line}, {colum}] -> ({matrix[line, colum]})");//если не выходят выводим значение
+        }
+        else
+            Console.Write($"[{line}, {colum}] -> такого элемента нет");// если выходят сообщаем пользователю об отсутствии такогт элемента
+    }
+    else
+        Console.Write("Введено неправильное значение");
+}
+
+Console.Write("Введите позицию элемента в ряду: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию элемента в столбце: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2D = CreateMatrixRndInt(4, 4, -10, 10);
+PrintMatrix(array2D);
+Console.WriteLine("");
+ReturnMeaning(array2D, row, column);
