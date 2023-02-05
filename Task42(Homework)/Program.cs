24b88a329ab6eebd647a867dc576a0d12593e988
Andreye <drontop1999@mail>
2023-02-05 21:44:18 +0300

    changed  Task42(Homework)

--- Task42(Homework)/Program.cs
+++ Task42(Homework)/Program.cs
@@ -19,7 +19,7 @@ int NumbersAboveZero(int[] arr)
     int sum = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] > 0) sum += arr[i];
+        if (arr[i] > 0) sum = arr[i];
     }
     return sum;
 }
