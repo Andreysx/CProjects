4828a803d833cc3da9e8c15e116605dccd6794b7
Andreye <drontop1999@mail>
2023-02-11 23:55:42 +0300

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,31 @@
+﻿// int FactorialRec(int n)
+//  {
+//      if (n == 1) return 1;
+//      else return n * FactorialRec(n - 1);
+// //  }
+
+//  Console.WriteLine(FactorialRec(10)); // 3628800
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+
+
+Console.Write("Введите первое число : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = DigitNumber(number1, number2);
+Console.Write(result);
+
+
+int DigitNumber(int num1, int num2)
+{
+    if (num2 == 0) return 1;
+    else return num1 *  DigitNumber(num1, num2 - 1);
+
+
+}
