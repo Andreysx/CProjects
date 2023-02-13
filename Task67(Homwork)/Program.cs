c53f1094aa4f4ddda75537a77a097a1a4f6be3d4
Andreye <drontop1999@mail>
2023-02-13 20:31:31 +0300

    Task67(Homwork)

---
+++ Task67(Homwork)/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// // N = 5 -> "5, 4, 3, 2, 1"
+// // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    Console.Write($"N = {number}-> \"");//\n перенос строки      \-
+    NaturalNumber(number);
+    Console.Write($"\"");
+
+}
+else Console.WriteLine("Введено некорректное значение");
+
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    if (num == 1) Console.Write($"{num} ");
+    else Console.Write($"{num}, ");
+
+    NaturalNumber(num - 1);
+}
+
+
+
